499ebdc0662f1aab26df08dde965bc7d08efd13e
Andrew Dunai <a@dun.ai>
2024-08-20 01:08:08 +0300

    Add pip readme

--- python/setup.py
+++ python/setup.py
@@ -4,9 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="fastmsg",
-    version="0.1.1",
+    version="0.1.1.post1",
     description="Set of minimalistic cross-language serialization libraries for C++ / Go / Python",
     packages=find_packages(),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     install_requires=[
         # 'six',
     ],
